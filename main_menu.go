c06bbb63e40bb95183b908f3fc047e464f3bbadb
kori-irrlicht <kori.irrlicht.dev@gmail.com>
2017-03-15 18:37:11 +0100

    Build menu in ui/menu.go

--- main_menu.go
+++ main_menu.go
@@ -13,7 +13,10 @@ import (
 type mainMenu struct{}
 
 func (g *mainMenu) Type() string { return sceneMainMenu }
-func (g *mainMenu) Preload()     {}
+func (g *mainMenu) Preload() {
+	err := engo.Files.Load("Roboto-Regular.ttf")
+	fmt.Println(err)
+}
 func (g *mainMenu) Setup(world *ecs.World) {
 
 	common.SetBackground(color.White)
@@ -22,40 +25,24 @@ func (g *mainMenu) Setup(world *ecs.World) {
 	world.AddSystem(&common.MouseSystem{})
 	world.AddSystem(&ui.ActionSystem{})
 
-	startButton := newButton(
-		func() {
-			fmt.Println("Start new game")
-			engo.SetSceneByName(sceneGame, false)
-		})
-	b1 := newButton(func() {
-		fmt.Println("b1")
-	})
-	b2 := newButton(func() {
-		fmt.Println("b2")
-	})
+	menu := buildMainMenu()
 
-	buttons := []*ui.Button{startButton, b1, b2}
-
-	buildMainMenu(buttons)
-
-	for _, ele := range buttons {
-		for _, s := range world.Systems() {
-			switch sys := s.(type) {
-			case *common.RenderSystem:
-				sys.Add(ele.BasicEntity, ele.RenderComponent, ele.SpaceComponent)
-			case *common.MouseSystem:
-				sys.Add(ele.BasicEntity, ele.MouseComponent, ele.SpaceComponent, ele.RenderComponent)
-			case *ui.ActionSystem:
-				sys.Add(ele.BasicEntity, ele.ActionComponent, ele.MouseComponent)
-			}
-		}
-
+	for _, s := range world.Systems() {
+		menu.AddTo(s)
 	}
 
 }
 
-func buildMainMenu(buttons []*ui.Button) {
-	m := ui.Menu{}
+func buildMainMenu() *ui.Menu {
+	buttons := []ui.Button{
+		{
+			Text: "Hallo",
+			Action: func() {
+				fmt.Println("test")
+			},
+		},
+	}
+	m := &ui.Menu{}
 	m.SpaceComponent = &common.SpaceComponent{
 		Position: engo.Point{X: 0, Y: 0},
 		Height:   100,
@@ -65,28 +52,8 @@ func buildMainMenu(buttons []*ui.Button) {
 		X: 400,
 		Y: 320,
 	})
-	for _, b := range buttons {
-		m.Add(b)
-	}
-	m.Build()
 
-}
-
-func newButton(action func()) *ui.Button {
-	e := ecs.NewBasic()
-	startButton := ui.Button{BasicEntity: &e}
-	startButton.RenderComponent = &common.RenderComponent{
-		Drawable: common.Rectangle{
-			BorderWidth: 1.,
-			BorderColor: color.RGBA{0, 255, 0, 255},
-		},
-		Hidden: false,
-	}
-	startButton.MouseComponent = &common.MouseComponent{}
-
-	startButton.ActionComponent = &ui.ActionComponent{}
-	startButton.Action = action
-
-	return &startButton
+	m.BuildMenu(buttons)
+	return m
 
 }
--- ui/menu.go
+++ ui/menu.go
@@ -1,32 +1,27 @@
 package ui
 
 import (
+	"fmt"
+	"image/color"
+
+	"engo.io/ecs"
 	"engo.io/engo"
 	"engo.io/engo/common"
 )
-
-//  Element is displayed in the UI
-type Element interface {
-	AddSpaceComponent(*common.SpaceComponent)
-}
 
 // Menu is a collection of UIElements arranged to build a menu
 // It only sets the values of the elements
 type Menu struct {
 	*common.SpaceComponent
-	elements []Element
-}
-
-//Add adds an Element to the menu
-func (m *Menu) Add(e Element) {
-	m.elements = append(m.elements, e)
+	buttons []buttonEntity
 }
 
 // Build arranges the elements relative to the location of the menu
 // Currently only vertical arrangement is supported
 // The SpaceComponent of the elements will be changed
-func (m *Menu) Build() {
-	amount := len(m.elements)
+func (m *Menu) positionEntries() {
+	amount := len(m.buttons)
+	fmt.Println(m)
 	part := (m.Height) / float32(amount)
 
 	for i := 0; i < amount; i++ {
@@ -38,16 +33,92 @@ func (m *Menu) Build() {
 				Y: m.Position.Y + float32(i)*part,
 			},
 		}
-		m.elements[i].AddSpaceComponent(sc)
+		m.buttons[i].SpaceComponent = sc
+	}
+}
+
+func (m *Menu) createButtons(buttons []Button) {
+	fnt := &common.Font{
+		URL:  "Roboto-Regular.ttf",
+		FG:   color.Black,
+		Size: 64,
+	}
+	err := fnt.CreatePreloaded()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, button := range buttons {
+		basic := ecs.NewBasic()
+		e := buttonEntity{}
+		e.BasicEntity = &basic
+		e.MouseComponent = &common.MouseComponent{}
+		e.parts = make([]*buttonPart, 2)
+		e.ActionComponent = &ActionComponent{}
+		e.Action = button.Action
+
+		borderPart := buttonPart{}
+		borderPart.RenderComponent = &common.RenderComponent{
+			Drawable: common.Rectangle{
+				BorderWidth: 1.,
+				BorderColor: color.RGBA{0, 255, 0, 255},
+			},
+			Hidden: false,
+		}
+		e.parts[0] = &borderPart
+
+		textPart := buttonPart{}
+		textPart.RenderComponent = &common.RenderComponent{
+			Drawable: common.Text{
+				Font: fnt,
+				Text: button.Text,
+			},
+		}
+		e.parts[1] = &textPart
+		m.buttons = append(m.buttons, e)
 	}
 
 }
 
-type Button interface {
-	Text() string
-	Action()
+type Button struct {
+	Text   string
+	Action func()
 }
 
-func BuildMenu(buttons []Button) (menu Menu) {
+type buttonEntity struct {
+	*ecs.BasicEntity
+	*common.SpaceComponent
+	*common.MouseComponent
+	*ActionComponent
+	parts []*buttonPart
+}
+
+type buttonPart struct {
+	*common.RenderComponent
+}
+
+//BuildMenu creates the menu from the given buttons
+func (m *Menu) BuildMenu(buttons []Button) {
+	m.buttons = make([]buttonEntity, 0)
+	m.createButtons(buttons)
+	m.positionEntries()
 	return
+}
+
+// AddTo adds the menu elements to the given system
+func (m *Menu) AddTo(system ecs.System) {
+	switch sys := system.(type) {
+	case *common.RenderSystem:
+		for _, ele := range m.buttons {
+			sys.Add(ele.BasicEntity, ele.parts[0].RenderComponent, ele.SpaceComponent)
+			sys.Add(ele.BasicEntity, ele.parts[1].RenderComponent, ele.SpaceComponent)
+		}
+	case *common.MouseSystem:
+		for _, ele := range m.buttons {
+			sys.Add(ele.BasicEntity, ele.MouseComponent, ele.SpaceComponent, ele.parts[1].RenderComponent)
+		}
+	case *ActionSystem:
+		for _, ele := range m.buttons {
+			sys.Add(ele.BasicEntity, ele.ActionComponent, ele.MouseComponent)
+		}
+	}
 }
